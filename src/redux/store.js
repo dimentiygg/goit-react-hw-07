657762460e5533c5e00c9afbbc09263809768e3c
Dmytro <az.36419.a@gmail.com>
2024-05-14 16:06:15 +0300

    delete persistor

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,37 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import contactsReducer from "./contactSlice";
 import filtersReducer from "./filterSlice";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
-
-const contactsPersistConfig = {
-  key: "contact",
-  storage,
-  whitelist: ["items"],
-};
-
-const pContactsReducer = persistReducer(contactsPersistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
-    contacts: pContactsReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
